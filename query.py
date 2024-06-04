b219df1042cfc29e4e55678bf87ad3484606865d
Dale Ferrario <daleferrario@gmail.com>
2024-06-03 21:13:41 -0700

    Changes to support chromadb

--- query.py
+++ query.py
@@ -1,33 +1,40 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, ServiceContext
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-from llama_index.vector_stores.chroma import ChromaVectorStore
-# from llama_index.storage.storage_context import StorageContext
-from llama_index import StorageContext
+from pathlib import Path
 import chromadb
+from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryReader, StorageContext
+# from llama_index.storage.storage_context import StorageContext
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+# from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, ServiceContext
+# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# from llama_index.llms.ollama import Ollama
+# from llama_index.vector_stores.chroma import ChromaVectorStore
+# from llama_index.storage.storage_context import StorageContext
+# from llama_index import StorageContext
+# import chromadb
 
 print("Starting")
 
 ## Embeddings
 # set embeddings model
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-# Create embeddings data
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+# Load data
 documents = SimpleDirectoryReader("data/mlg_data").load_data()
 
-## ChromaDB
-# create chromadb client and collection
+# Create ChromaDB
 chroma_client = chromadb.EphemeralClient()
 chroma_collection = chroma_client.create_collection("mlg_data")
-# set up ChromaVectorStore and load in data
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+## Initialize Ollama and ServiceContext
+llm = Ollama(model="tinydolphin", request_timeout=600.0)
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model)
 
 ## Llama Index
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-service_context = ServiceContext.from_defaults(embed_model=embed_model)
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, service_context=service_context)
-
-## Ollama
-Settings.llm = Ollama(model="tinydolphin", request_timeout=600.0)
+query_engine = index.as_query_engine()
 
 ## Query
 print("RESPONSE")
