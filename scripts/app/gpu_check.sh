3d2b0b5f2861b4f443a38e8cc62ab272e3cb533b
Anthony <anthonylaiferrario@gmail.com>
2024-06-28 10:02:08 -0700

    run shared services

---
+++ scripts/app/gpu_check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Initialize NO_GPU variable
+export NO_GPU="-no-gpu"
+
+# Check if nvidia-smi is installed
+if command -v nvidia-smi &> /dev/null; then
+    # Run nvidia-smi and capture the output
+    gpu_info=$(nvidia-smi 2>&1)
+    
+    # Check if nvidia-smi command ran successfully
+    if [ $? -eq 0 ]; then
+        # Check if GPU is detected
+        if echo "$gpu_info" | grep -q "NVIDIA-SMI"; then
+            echo "NVIDIA GPU detected."
+            NO_GPU=""
+        else
+            echo "No NVIDIA GPU detected."
+        fi
+    else
+        echo "nvidia-smi command failed."
+    fi
+else
+    echo "nvidia-smi is not installed."
+fi
+
+export NO_GPU
+
+# Output the value of NO_GPU variable
+echo "NO_GPU is set to: $NO_GPU"
---
+++ scripts/app/run_shared_services.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+set -e
+
+# Directory paths
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+STATE_DIR="$(dirname $(dirname $SCRIPT_DIR))/state"
+ROOT_DIR=$(dirname $(dirname $SCRIPT_DIR))
+
+# Function to display usage
+usage() {
+  echo "Usage: $0 -n <deployment-name>"
+  exit 1
+}
+
+# Parse command-line arguments
+echo "Arguments:"
+while getopts ":n:" opt; do
+  echo "-$opt $OPTARG"
+  case $opt in
+    n)
+      DEPLOYMENT_NAME="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+# Check if mandatory arguments are provided
+if [ -z "$DEPLOYMENT_NAME" ]; then
+  usage
+fi
+
+# Check if .state file exists
+echo "Collecting .state file"
+STATE_PATH="$STATE_DIR/$DEPLOYMENT_NAME.state"
+if [ ! -f "$STATE_PATH" ]; then
+  echo "$DEPLOYMENT_NAME.state not found!"
+  usage
+  exit 1
+fi
+source $STATE_PATH
+
+# Check if mandatory arguments are provided
+if [ -z "$DEPLOYMENT_NAME" ] || [ -z "$REGION" ] || [ -z "$KEY_FILE_PATH" ]; then
+  usage
+fi
+
+# Get the instance ID from the stack resource
+INSTANCE_ID=$(aws cloudformation describe-stack-resources \
+  --stack-name "$DEPLOYMENT_NAME" \
+  --logical-resource-id "WebServerInstance" \
+  --query "StackResources[0].PhysicalResourceId" \
+  --output text \
+  --region "$REGION")
+
+# Check if the instance ID was found
+if [ -z "$INSTANCE_ID" ]; then
+  echo "Instance ID for resource '$RESOURCE_LOGICAL_ID' in stack '$DEPLOYMENT_NAME' not found."
+  exit 1
+fi
+
+echo "Instance ID: $INSTANCE_ID"
+
+# Get the public DNS of the instance
+URL=$(aws ec2 describe-instances \
+  --instance-ids "$INSTANCE_ID" \
+  --region "$REGION" \
+  --query "Reservations[0].Instances[0].PublicDnsName" \
+  --output text)
+
+echo "URL of instance: $URL"
+
+# Copying files
+echo "Copying docker compose files to instance."
+scp -o "StrictHostKeyChecking=no" -i "$KEY_FILE_PATH" \
+"$ROOT_DIR/docker-compose-shared.yml" \
+"$ROOT_DIR/docker-compose-shared-no-gpu.yml" \
+"$SCRIPT_DIR/gpu_check.sh" \
+"ubuntu@$URL:/home/ubuntu/"
+
+INITIAL_COMMANDS=$(cat <<EOF
+source ./gpu_check.sh
+docker compose -f "docker-compose-shared\$NO_GPU.yml" up -d
+EOF
+)
+
+# Execute the initial commands on the remote server via SSH
+echo "Launching shared services on $DEPLOYMENT_NAME"
+ssh -t -o "StrictHostKeyChecking=no" -i "$KEY_FILE_PATH" "ubuntu@$URL" <<EOF
+$INITIAL_COMMANDS
+EOF
