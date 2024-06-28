d8f0683f9c0e3e8fe6d1ca5277fe8b07d470a079
Anthony <anthonylaiferrario@gmail.com>
2024-06-27 23:24:40 -0700

    switching to docker compose v2 syntax

--- scripts/make.sh
+++ scripts/make.sh
@@ -3,5 +3,5 @@
 # Get the directory of the script
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 
-docker-compose -f $SCRIPT_DIR/docker-compose-build.yml build --parallel
-docker-compose -f $SCRIPT_DIR/docker-compose-build.yml push
+docker compose -f $SCRIPT_DIR/docker-compose-build.yml build --parallel
+docker compose -f $SCRIPT_DIR/docker-compose-build.yml push
--- test/local_test.sh
+++ test/local_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Get the directory of the script
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 echo $SCRIPT_DIR
@@ -7,8 +9,8 @@ echo $SCRIPT_DIR
 # Function to clean up background processes
 cleanup() {
     echo "Stopping services and logging..."
-    docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" down -v
-    docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" down -v
+    docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" down -v
+    docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" down -v
     kill $SHARED_LOG_PID
     kill $CUSTOMER_LOG_PID
     echo "Cleanup complete."
@@ -19,18 +21,18 @@ trap cleanup SIGINT SIGTERM
 
 # Clean environment
 echo "Cleaning up environment"
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" down -v
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" down -v
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" down -v
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" down -v
 
 # Pull latest images
 echo "Pulling latest images"
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" pull
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" pull
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" pull
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" pull
 
 # Deploy shared services
 echo "Deploying shared services"
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" up -d
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" logs -f > "$SCRIPT_DIR/docker-compose-shared.log" &
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" up -d
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-shared.yml" logs -f > "$SCRIPT_DIR/docker-compose-shared.log" &
 SHARED_LOG_PID=$!
 echo "Waiting for 'ollama' service to be up and responding..."
 until docker exec ollama ollama ps &> /dev/null; do
@@ -41,8 +43,8 @@ echo "'ollama' service is up and responding."
 
 # Load customer data
 echo "Loading data"
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" up data_loader -d
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" logs -f > "$SCRIPT_DIR/docker-compose-customer.log" &
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" up data_loader -d
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" logs -f > "$SCRIPT_DIR/docker-compose-customer.log" &
 CUSTOMER_LOG_PID=$!
 while docker ps | grep -q "data_loader"; do
     echo "Waiting for 'data_loader' to exit..."
@@ -52,8 +54,8 @@ echo "data_loader has exited."
 
 # Deploy customer query_server
 echo "Deploying query_server"
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" up query_server -d
-docker-compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" logs -f >> "$SCRIPT_DIR/docker-compose-customer.log" &
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" --env-file "$SCRIPT_DIR/.env.dgm_test" up query_server -d
+docker compose -f "$(dirname "$SCRIPT_DIR")/docker-compose-customer.yml" logs -f >> "$SCRIPT_DIR/docker-compose-customer.log" &
 
 # Wait for all background logging processes to end
 wait $SHARED_LOG_PID
