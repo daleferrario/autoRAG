af72a364492c869b568c3a1b388fc1f078627d35
Dale Ferrario <daleferrario@gmail.com>
2024-06-28 17:32:43 -0700

    fix make paths and pull first

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,6 +2,8 @@
 
 # Get the directory of the script
 SCRIPT_DIR=$(dirname $(realpath "$0"))
+ROOT_DIR=$(dirname "$SCRIPT_DIR")
 
-docker compose -f $SCRIPT_DIR/docker-compose-build.yml build --parallel
-docker compose -f $SCRIPT_DIR/docker-compose-build.yml push
+docker compose -f $ROOT_DIR/docker-compose-build.yml pull
+docker compose -f $ROOT_DIR/docker-compose-build.yml build --parallel
+docker compose -f $ROOT_DIR/docker-compose-build.yml push
