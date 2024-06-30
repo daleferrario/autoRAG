2d54212d296ee6e2aa72c107df07c0b0a52bef69
Anthony <anthonylaiferrario@gmail.com>
2024-06-30 02:03:06 -0700

    improve rootdir

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,7 +2,7 @@
 
 # Get the directory of the script
 SCRIPT_DIR=$(dirname $(realpath "$0"))
-ROOT_DIR=$(dirname "$SCRIPT_DIR")
+ROOT_DIR=$(git -C "$SCRIPT_DIR" rev-parse --show-toplevel 2>/dev/null)
 
 docker compose -f $ROOT_DIR/docker-compose-build.yml pull
 docker compose -f $ROOT_DIR/docker-compose-build.yml build --parallel
